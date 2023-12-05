0bdd55ccf1a533940bcae458251512ff56c8875b
maiaJhfB <125679583+maiaJhfB@users.noreply.github.com>
2023-12-04 23:33:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
 const axios = require("axios");
-//sua chave aqui
-const appid = "7b2cd3cc5568e58b3ddb64196db91746";
-//cidade desejada
-const q = "Itu";
-//unidade de medida de temperatura
- const units = "metric";
- //idioma
-const lang = "pt_BR";
-//quantidade de resultados
-const cnt = "10"
-const url = `https://api.openweathermap.org/data/2.5/forecast?q=${q}&units=${units}&appid=${appid}&lang=${lang}&cnt=${cnt}`;
-//faz a requisição
- axios
- .get(url)
- .then((res) => {
- //mostra o resultado e devolve somente a parte de interesse
-console.log(res);
-return res.data;
- })
- .then((res) => {
- //mostra o total e devolve o resultado
- console.log(res.cnt);
- return res;
- })
- .then((res) => {
- //devolve somente a lista de previsões
- console.log("aqui", res);
- return res["list"];
- })
- .then((res) => {
- //para cada resultado, mostra algumas informações
- for (let previsao of res) {
- console.log(`
- ${new Date(+previsao.dt * 1000).toLocaleString()},
- ${'Min: ' + previsao.main.temp_min}\u00B0C,
- ${'Max: ' + previsao.main.temp_max}\u00B0C,
- ${'Hum: ' + previsao.main.humidity}%,
- ${previsao.weather[0].description}
- `);
- }
- return res;
- })
-.then((res) => {
- //verifica quantas previsões têm percepção humana de tempertura acima de 30 graus
-const lista = res.filter(r => r.main.feels_like >= 30);
-console.log (`${lista.length} previsões têm percepção humana de temperatura acima de 30 graus`)
-});
+cidade = "New York";
+limit = 1.0;
+appid = "7b2cd3cc5568e58b3ddb64196db91746";
+const url = (`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=${limit}&appid=${appid}`);
+axios
+    .get(url)
+    .then((res) => {
+        console.log(res);
+        return res.data;
+    })
+    .then((res) => {
+        for (let coordenadas of res) {
+         console.log(`
+         ${'longitude: ' + coordenadas.lon}, ${'Latitude: ' + coordenadas.lat},`);
+         const longitude = coordenadas.lon
+         const latitude = coordenadas.lat
+         }
+         return res;
+         })
